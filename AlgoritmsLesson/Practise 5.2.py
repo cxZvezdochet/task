d7ea91dfafcfafb22d3b7d487347936e14dfbaae
cxZvezdochet <145109889+cxZvezdochet@users.noreply.github.com>
2024-01-25 16:49:48 +0300

    Delete AlgoritmsLesson directory

--- AlgoritmsLesson/Practise 5.2.py
+++
@@ -1,9 +0,0 @@
-#Задача 4
-import heapq
-
-def klargest(arr,n,k):
-    # arr = [-i for i in arr] если нужно найти максимальное
-    heapq.heapify(arr)
-    for _ in range(k):
-        print(heapq.heappop(arr))
-klargest([1,23,12,9,30,2, 50], 7,3)
